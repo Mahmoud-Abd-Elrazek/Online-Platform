9c1f617332786229b8c947042e0b572407bb70c5
Mahmoud Abdelrazek <mahmoudzanitty@gmail.com>
2025-02-13 12:23:19 +0200

    Log in and log out process

---
+++ app.js
@@ -0,0 +1,132 @@
+const baseUrl = "https://tarmeezacademy.com/api/v1";
+var requestOptions = {
+   method: 'GET',
+   redirect: 'follow'
+};
+
+fetch(`${baseUrl}/posts?limit=10`, requestOptions)
+   .then(response => response.json())
+   .then(posts => {
+      document.getElementById("posts").innerHTML = "";
+      for (const post of posts.data) {
+         // todo: Add Tags here
+         document.getElementById("posts").innerHTML += createPost(post);
+      }
+   })
+   .catch(error => console.log('error', error));
+
+// Function To Create Post and Push into the page
+function createPost(post) {
+   let postImage = post.image;
+   let userImage = post.author.profile_image;
+   if (typeof post.image === "object")
+      postImage = "./placeholders/2.jpg";
+
+   if (typeof post.author.profile_image === "object")
+      userImage = "./profile-pics/user-4.png";
+
+   return `
+      <div class="card shadow-sm">
+         <div class="card-header">
+            <img src='${userImage}' style="width: 40px; height: 40px;" alt="profile picture"
+               class="rounded-circle border-3">
+            <b>@${post.author.username}</b>
+         </div>
+
+         <div class="card-body">
+            <img src='${postImage}' alt="post img" class="img-fluid">
+            <h6 class="mt-1" style="color: rgb(58, 56, 56);">${post.created_at}</h6>
+            <h5>${post.title || "Empty"}</h5>
+            <p>${post.body}</p>
+
+            <hr>
+            <div>
+               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi bi-pen" viewBox="0 0 16 16">
+                  <path
+                     d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z" />
+               </svg>
+               <span>
+                  ( ${post.comments_count} )Comments
+                  <span>
+                     <button class = "btn btn-sm rounded-5" style="background:gray; color:#fff"> tag </button>
+                  </span>   
+               </span>
+            </div>
+         </div>
+      </div>
+   `
+}
+
+function loginBtnClicked() {
+   const username = document.getElementById("username-input").value;
+   const password = document.getElementById("password-input").value;
+
+   const params = {
+      "username": username,
+      "password": password
+   }
+   const url = "https://tarmeezacademy.com/api/v1/login";
+   axios.post(url, params)
+      .then((response) => {
+         localStorage.setItem("token", response.data.token);
+         localStorage.setItem("user", JSON.stringify(response.data.user));
+
+         const modal = document.getElementById("login-modal");
+         const modalInstance = bootstrap.Modal.getInstance(modal);
+         modalInstance.hide();
+         setupUI();
+         shwoalertPlaceholder("Logged in successfully");
+      })
+}
+
+function logout() {
+   localStorage.removeItem("user");
+   localStorage.removeItem("token");
+
+   setupUI();
+   shwoalertPlaceholder("Logged out successfully");
+}
+
+function setupUI() {
+   const token = localStorage.getItem("token");
+
+   const loginDiv = document.getElementById("logged-in-div");
+   const logoutBtn = document.getElementById("logout-div");
+
+   if (token == null) { // gust 
+      loginDiv.setAttribute('style', 'display:flex !important');
+      logoutBtn.setAttribute('style', 'display:none !important');
+   }
+   else {
+      loginDiv.setAttribute('style', 'display:none !important');
+      logoutBtn.setAttribute('style', 'display:flex !important');
+   }
+} setupUI(); // call it directly 'Runnig All Time'
+
+
+function shwoalertPlaceholder(alertMessage) {
+   const alertPlaceholder = document.getElementById("success-alert");
+   // Create the div element
+
+   let temp = null; // this is a pointer to point the alret div to hide from the DOM without delete the original node
+   const appendAlert = (message, type) => {
+      const wrapper = document.createElement('div')
+      wrapper.innerHTML = [
+         `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+         `   <div>${message}</div>`,
+         '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+         '</div>'
+      ].join('')
+
+      alertPlaceholder.append(wrapper)
+      temp = wrapper; // save the alret element
+   }
+   appendAlert(alertMessage, 'success');
+
+   // todo: hide the alert
+   setTimeout(() => {
+      temp.setAttribute('style', 'display:none !important'); // after 2 hide the alret without removing 
+   }, 2000);
+
+}
